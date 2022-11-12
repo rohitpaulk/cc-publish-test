d29c7ed7c5143cef877a77eb17ac3b01265416fe
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-11-12 21:20:11 +0000

    changes

--- app/server.go
+++ app/server.go
@@ -13,15 +13,20 @@ func main() {
 	 	os.Exit(1)
 	 }
 
-	 conn, err := l.Accept()
-	 if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	 for {
+		 conn, err := l.Accept()
+		 if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		 }
+		 go handleConn(conn)
 	 }
+}
 
-
+func handleConn(conn net.Conn) {
 	 for {
 		 conn.Read(make([]byte, 1024));
 		 conn.Write([]byte("+PONG\r\n"))
 	 }
 }
+
